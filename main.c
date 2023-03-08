ba022002da9f863f6e93f6dd3f238e7d2f73227a
FabioLopes021 <a25999@alunos.ipca.pt>
2023-03-08 23:46:03 +0000

    login reduzido

--- main.c
+++ main.c
@@ -8,12 +8,16 @@
 #include "gestores.c"
 
 
+// Funçao de apoio as funçoes de Login, serve para reduzir o menu para apenas uma opçao de login
+int funcaoLogin(Clientes **inicio, Gestores **g, char password[], char email[], Clientes **logc, Gestores **logg);
+
+
 int main(){
     Meio *h = NULL;
     Clientes *c = NULL, *loginc = NULL;
     Gestores *g = NULL, *loging = NULL;
-
-
+    int logmenu = 0;
+    
     char nome[MAX_NAME], morada[MAX_MORADA], NIF[MAX_NIF], password[MAX_PASSWORD], email[MAX_EMAIL];
     char emaillog[MAX_EMAIL], passwordlog[MAX_PASSWORD];
     float saldo;
@@ -37,7 +41,8 @@ int main(){
     saldo = 9530.67;
     
     //inserirCliente(&c,nome,morada,NIF,saldo,password,email);
-    inserirGestor(&g, nome, morada, NIF, password, email);
+    //inserirGestor(&g, nome, morada, NIF, password, email);
+    inserirCliente(&c, nome, morada, NIF, saldo, password, email);
 
     printf("\n---------LOGIN--------\n");
     printf("indique o seu email: ");
@@ -48,15 +53,50 @@ int main(){
     fgets(passwordlog,MAX_PASSWORD,stdin);
     passwordlog[strlen(passwordlog) - 1] = '\0';
 
-
-    loging = loginGestores(&g, passwordlog, emaillog);
-    if ( loging == NULL){
-        printf("\nOcorreu um erro no login, password ou email errados...\n");
-    }else{
-        puts(loging->name);
+    // teste login unico
+    logmenu = funcaoLogin(&c, &g, passwordlog, emaillog, &loginc, &loging);
+    
+    switch(logmenu){
+        case 1:
+            if (loginc != NULL){
+                printf("Login efetuado com sucesso. (cliente: %s)\n", loginc->name);
+            }else{
+                printf("Password ou email incorretos...\n");
+            }
+            break;
+        case 2:
+            if (loging != NULL){
+                printf("Login efetuado com sucesso. (cliente: %s)\n", loginc->name);
+            }else{
+                printf("Password ou email incorretos...\n");
+            }
+            break;
+        case 0:
+            break;
     }
-
 
 
     return 1;
 }
+
+
+
+
+int funcaoLogin(Clientes **inicio, Gestores **g, char password[], char email[], Clientes **logc, Gestores **logg){
+    int i = 0;
+
+
+    if (existeClienteEmail((*inicio), email)){
+        i = 1;
+        *logc = loginClientes(&(*inicio), password, email);
+    }else if(existeGestorEmail((*g), email)){
+        i = 2;
+        *logg = loginGestores(&(*g), password, email);
+    } else {
+        printf("Email ou password errados...\n");
+        return 0;
+    }
+
+    return i;
+}
+
