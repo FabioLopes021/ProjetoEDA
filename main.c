5f5735c7b0fd9972c1f336b54143dbc373981f14
FabioLopes021 <a25999@alunos.ipca.pt>
2023-03-08 13:43:46 +0000

    primeiro esquema de menus

--- main.c
+++ main.c
@@ -7,56 +7,110 @@
 #include "gestores.h"
 #include "gestores.c"
 
+int menu();
+int menuclientes();
+int menucGestores();
+
 
 int main(){
     Meio *h = NULL;
     Clientes *c = NULL, *loginc = NULL;
     Gestores *g = NULL, *loging = NULL;
+    int menu1, menuc, menug;
 
 
     char nome[MAX_NAME], morada[MAX_MORADA], NIF[MAX_NIF], password[MAX_PASSWORD], email[MAX_EMAIL];
     char emaillog[MAX_EMAIL], passwordlog[MAX_PASSWORD];
     float saldo;
 
-    strcpy(nome,"Fabio Lopes");
-    strcpy(morada,"Rua da Penide n236 Areias S Vicente, Barcelos");
-    strcpy(NIF, "12345678");
-    strcpy(password, "fabio");
-    strcpy(email, "fabiolopes021@gmail.com");
-    saldo = 530.67;
-    
-    //inserirCliente(&c,nome,morada,NIF,saldo,password,email);
-    inserirGestor(&g, nome, morada, NIF, password, email);
+    do{
+        menu1 = menu();
+        switch(menu1){
+            case 1:
+                do{
+                    menuc = menuclientes();
+                    switch(menuc){
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 0:
+                            printf("Loging out...");
+                    }
+                }while(meuc != 0)
+                
+                break;
+            case 2:
+                menug
 
-
-    strcpy(nome,"Andre Lopes");
-    strcpy(morada,"Rua da Penide n236 Areias S Vicente, Barcelos");
-    strcpy(NIF, "87654321");
-    strcpy(password, "andre");
-    strcpy(email, "andrelopes021@gmail.com");
-    saldo = 9530.67;
-    
-    //inserirCliente(&c,nome,morada,NIF,saldo,password,email);
-    inserirGestor(&g, nome, morada, NIF, password, email);
-
-    printf("\n---------LOGIN--------\n");
-    printf("indique o seu email: ");
-    fgets(emaillog,MAX_EMAIL,stdin);
-    emaillog[strlen(emaillog) - 1] = '\0';
-
-    printf("Indique a sua password: ");
-    fgets(passwordlog,MAX_PASSWORD,stdin);
-    passwordlog[strlen(passwordlog) - 1] = '\0';
-
-
-    loging = loginGestores(&g, passwordlog, emaillog);
-    if ( loging == NULL){
-        printf("\nOcorreu um erro no login, password ou email errados...\n");
-    }else{
-        puts(loging->name);
-    }
-
+                break;
+            case 3:
+            
+                break;
+            case 0:
+                printf("Deseja guardar as alteraçoes ?");
+                break;
+        }
+    }while(menu1 != 0);
 
 
     return 1;
+}
+
+
+
+
+
+int menu(){
+    int opc;
+
+    do{
+        printf("\n ------------------------------\n");
+        printf("| 1 - login clientes           |\n");
+        printf("| 2 - login gestores           |\n");
+        printf("| 3 - Criar conta              |\n");
+        printf("| 0 - Sair                     |\n");
+        printf(" ------------------------------\n");
+        printf("Opcao: ");
+        scanf("%d", &opc);
+    }while(opc < 0 || opc > 3);
+    
+    return opc;
+}
+
+int menuclientes(){
+    int opc;
+
+    do{
+        printf("\n ------------------------------\n");
+        printf("| 1 - Alugar meio              |\n");
+        printf("| 2 - Alterar dados            |\n");
+        printf("| 0 - Logout                   |\n");
+        printf(" ------------------------------\n");
+        printf("Opcao: ");
+        scanf("%d", &opc);
+    }while(opc < 0 || opc > 2);
+
+    return opc;
+}
+
+int menucGestores(){
+    int opc;
+
+    do{
+        printf("\n ------------------------------\n");
+        printf("| 1 - Adicionar meios          |\n");
+        printf("| 2 - Consultar historico      |\n");
+        printf("| 3 - Estatisticas             |\n");
+        printf("| 4 - Validaçoes               |\n");
+        printf("| 5 - Alterar meios            |\n");
+        printf("| 5 - Alterar dados            |\n");
+        printf("| 0 - Logout                   |\n");
+        printf(" ------------------------------\n");
+        printf("Opcao: ");
+        scanf("%d", &opc);
+    }while(opc < 0 || opc > 5);
+
+    return opc;
+
 }
