6ea855222692660267f47e46b257001a279a7853
FabioLopes021 <a25999@alunos.ipca.pt>
2023-06-04 11:44:02 +0100

    fix pull

--- historico.h
+++ historico.h
@@ -17,26 +17,36 @@ typedef struct xhistorico{       // Declaraçao da estrutura de Clientes
 
 
 void inserirHistorico(Historico** inicio, int idc, int idm, int ide, double custof, float custo, char localinicial[], char localfinal[], struct tm start, struct tm end);
+
 void inserirHistoricoInicio(Historico** inicio, int idc, int idm, float custo, char localinicial[]);
+
 int inserirHistoricoFinal(Historico* inicio, int ide, char localfinal[]);
+
 Historico *removerHistorico(Historico **inicio, int id, int i);
+
 int existeHistorico(Historico* inicio, int id);
+
 double calculoCustoTotal(Historico *entrada, int ide);
+
 float calculoDist(Historico *entrada, VerticeList *v, int ide);
+
 int idEntrada(Historico *entrada, int idm);
+
 int generateidHistorico(Historico *inicio);
+
 int imprimirHistorico(Historico *inicio,VerticeList *v);
+
 int imprimirHistoricoCliente(Historico *inicio,VerticeList *v, int id);
+
 int numEntradasCliente(Historico *inicio, int id);
+
 void readHistorico(Historico **inicio);
+
 void guardarHistorico(Historico* inicio);
+
 void lerHistoricoBin(Historico **inicio);
+
 void guardarHistoricoBin(Historico *inicio);
-
-
-
-
-
 
 
 #endif //final da guarda de ficheiro
